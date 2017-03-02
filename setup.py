338e0ada773dd34b21ed4ba68bdd513c8883c877
marhamil723 <mhamilton723@gmail.com>
2017-03-01 20:34:50 -0500

    upgrading version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tflab',
-      version='0.1',
+      version='0.1.1',
       description='A laboratory for experimenting with Tensorflow sbstraction',
       url='https://github.com/mhamilton723/tflab',
       author='Mark Hamilton',
